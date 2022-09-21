0790a9c9859db90b40fec2e67a8f2b896dec0d95
Jasur Jaxongirov <jaxongirovjasurbek@gmail.com>
2022-09-21 16:39:49 +0500

    feat: create guess number game

--- lib/control flow and functions/control_flow_&_functions.dart
+++ lib/control flow and functions/control_flow_&_functions.dart
@@ -4,4 +4,56 @@
 ///
 /// Lab Assignment
 
+enum ProcessStatus {pure, inProgress, progressSucced, progressFailed}
+
+void main(List<String> args) {
+  printNumbers(30);
+}
+
+// TOOD: nol sonidan boshlab, kiritilgan son orasidagi barcha sonlarni console ga chiqaring
+void printNumbers(int number) {
+  for (var index = 0; index < number; index++) {
+    print(index);
+  }
+}
+
+
+// ------------------------------------------------------------------- //
+// TOOD: nol sonidan boshlab, kiritilgan son orasidagi barcha sonlarning yig'indisini consolega chiqaring
+// void printSumOfNumbers(int number) {
+  
+// }
+// ------------------------------------------------------------------- //
+
+// ------------------------------------------------------------------- //
+// TOOD: bir sonidan boshlab, kiritilgan son orasidagi barcha sonlarning ko'paytmasini consolega chiqaring
+// void printMultiplicationOfNumbers(int number) {
+  
+// }
+// ------------------------------------------------------------------- //
+
+// ------------------------------------------------------------------- //
+// TOOD: agar list orasida foydalanuvchi qidirayotgan ma'lumot bo'lsa, shu ma'lumotni consolega chiqaring va loopni to'xtating
+// HINT: break keywordidan foydalaning
+// void findItem(String item, List<String> list) {
+  
+// }
+// ------------------------------------------------------------------- //
+
+// ------------------------------------------------------------------- //
+// TOOD: Berilgan process statusiga mosravishda consolega yozuv chiqaring. Dastur tuzish davomida switchdan foydalaning
+// HINT: ProcessStatus enum o'zgaruvchisidan foydalaning
+// void printStatus(ProcessStatus status) {
+  
+// }
+// ------------------------------------------------------------------- //
+
+
+// ------------------------------------------------------------------- //
+// TOOD: Kiritilgan ma'lumotga ko'ra foydalanuvchining jinsini chiqarib beradigan dastur yozing. Bunda if else statementlarini va ternary operatorini ishlating
+// void printGender(bool isMale) {
+  
+// }
+// ------------------------------------------------------------------- //
+
 
--- lib/game/random_number.dart
+++ lib/game/random_number.dart
@@ -1,8 +1,36 @@
 /// O'ylangan sonni topish uchun o'yin
-/// 
-/// Warmup activity 
+///
+/// Warmup activity
 
+import 'dart:io';
+import 'dart:math';
 
 void main(List<String> args) {
-  
+  print("Men o'ylagan raqamni topish uchun biror raqam kiriting (0, 50):");
+  String? input = stdin.readLineSync();
+
+  if (input != null) {
+    int? parsedNumber = int.tryParse(input);
+
+    if (parsedNumber == null) {
+      print("Siz kiritgan raqam yaroqli emas!");
+    } else {
+      final int randomNumber = generateRandomNumber();
+
+      if (randomNumber == parsedNumber) {
+        print("Urra, siz men o'ylagan sonni topdingiz!");
+      } else {
+        print(
+          "Afsuski, siz yangilishdingiz!\n Men o'ylagan raqam $randomNumber edi",
+        );
+      }
+    }
+  } else {
+    print("Hech qanday raqam kiritilmadi!");
+  }
+}
+
+int generateRandomNumber() {
+  final randomObject = Random();
+  return randomObject.nextInt(50);
 }
